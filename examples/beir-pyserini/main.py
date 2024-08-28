5a8ba86877660c928d02809dd17664aced04fcd8
wang.steven.h@gmail.com <wang.steven.h@gmail.com>
2024-08-29 00:57:24 +0200

    Update pyserini dockerimage to allow multiple indices

--- examples/beir-pyserini/main.py
+++ examples/beir-pyserini/main.py
@@ -20,11 +20,9 @@ async def upload(file: UploadFile = File(...)):
 
 @app.get("/index/")
 def index(index_name: str, threads: Optional[int] = 8):
-    settings.index_name = index_name
-
     command = f"python -m pyserini.index -collection JsonCollection \
     -generator DefaultLuceneDocumentGenerator -threads {threads} \
-    -input {settings.data_folder} -index {settings.index_name} -storeRaw \
+    -input {settings.data_folder} -index {index_name} -storeRaw \
     -storePositions -storeDocvectors"
     
     os.system(command)
@@ -32,47 +30,26 @@ def index(index_name: str, threads: Optional[int] = 8):
     return {200: "OK"}
 
 @app.get("/lexical/search/")
-def search(q: str, 
+def search(index_name: str,
+           q: str,
            k: Optional[int] = 1000,
            bm25: Optional[Dict[str, float]] = {"k1": 0.9, "b": 0.4},
-           fields: Optional[Dict[str, float]] = {"contents": 1.0, "title": 1.0}):
-    
-    searcher = SimpleSearcher(settings.index_name)
+           fields: Optional[Dict[str, float]] = {"contents": 1.0, "title": 1.0},
+           ) -> Dict[str, float]:
+    searcher = SimpleSearcher(index_name)
     searcher.set_bm25(k1=bm25["k1"], b=bm25["b"])
-    
     hits = searcher.search(q, k=k, fields=fields)
-    results = []
-    for i in range(0, len(hits)):
-        results.append({'docid': hits[i].docid, 'score': hits[i].score})
-
-    return {'results': results}
+    return {hit.docid: hit.score for hit in hits}
 
 @app.post("/lexical/batch_search/")
-def batch_search(queries: List[str],
+def batch_search(index_name: str,
+                 queries: List[str],
                  qids: List[str],
                  k: Optional[int] = 1000, 
                  threads: Optional[int] = 8, 
                  bm25: Optional[Dict[str, float]] = {"k1": 0.9, "b": 0.4}, 
-                 fields: Optional[Dict[str, float]] = {"contents": 1.0, "title": 1.0}):
-    
-    searcher = SimpleSearcher(settings.index_name)
+                 fields: Optional[Dict[str, float]] = {"contents": 1.0, "title": 1.0}) -> Dict[str, Dict[str, float]]:
+    searcher = SimpleSearcher(index_name)
     searcher.set_bm25(k1=bm25["k1"], b=bm25["b"])
-    
     hits = searcher.batch_search(queries=queries, qids=qids, k=k, threads=threads, fields=fields)
-    return {'results': config.hit_template(hits)}
-
-@app.post("/lexical/rm3/batch_search/")
-def batch_search_rm3(queries: List[str], 
-                     qids: List[str], 
-                     k: Optional[int] = 1000, 
-                     threads: Optional[int] = 8, 
-                     bm25: Optional[Dict[str, float]] = {"k1": 0.9, "b": 0.4}, 
-                     fields: Optional[Dict[str, float]] = {"contents": 1.0, "title": 1.0},
-                     rm3: Optional[Dict[str, Union[int, float]]] = {"fb_terms": 10, "fb_docs": 10, "original_query_weight": 0.5}):
-    
-    searcher = SimpleSearcher(settings.index_name)
-    searcher.set_bm25(k1=bm25["k1"], b=bm25["b"])
-    searcher.set_rm3(fb_terms=rm3["fb_terms"], fb_docs=rm3["fb_docs"], original_query_weight=rm3["original_query_weight"])
-    
-    hits = searcher.batch_search(queries=queries, qids=qids, k=k, threads=threads, fields=fields)
-    return {'results': config.hit_template(hits)}
+    return config.hit_template(hits)
